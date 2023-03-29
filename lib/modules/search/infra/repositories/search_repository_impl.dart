bb2675d043a8c62eaea171dfef6bfe84baf48b7c
Juliana Cavalaro <44675486+Julianacavalaro@users.noreply.github.com>
2023-03-29 11:57:37 -0300

    commit de teste 2

---
+++ lib/modules/search/infra/repositories/search_repository_impl.dart
@@ -0,0 +1,19 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:github_search/modules/search/domain/errors/errors.dart';
+import 'package:github_search/modules/search/domain/repositories/search_repository.dart';
+import 'package:github_search/modules/search/infra/repositories/datasource/search_datasource.dart';
+
+class SearchRepositoryImpl implements SearchRepository{
+  final SearchDataSource datasource;
+
+  SearchRepositoryImpl(this.datasource);
+  
+  @override
+  Future<Either<FailureSearch, List<ResultSearch?>?>?>? search(String? searchText) {
+    // TODO: implement search
+    throw UnimplementedError();
+  }
+  
+}
