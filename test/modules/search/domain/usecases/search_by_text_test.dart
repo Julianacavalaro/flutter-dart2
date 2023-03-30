44fe42077d79092dad79035f8102b09e76032b28
Juliana Cavalaro <juliana.cavalaro@hotmail.com>
2023-03-30 19:44:42 -0400

    Clean Architecture #2 final Teste unitario

--- test/modules/search/domain/usecases/search_by_text_test.dart
+++ test/modules/search/domain/usecases/search_by_text_test.dart
@@ -1,7 +1,7 @@
-
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:github_search/modules/search/domain/errors/errors.dart';
 import 'package:github_search/modules/search/domain/repositories/search_repository.dart';
 import 'package:github_search/modules/search/domain/usecases/search_by_text.dart';
 import 'package:mockito/mockito.dart';
@@ -34,6 +34,11 @@ void main() {
         .thenAnswer((_) async => Right(<ResultSearch>[]));
 
     var result = await usecase(null);
+    // expect(result!.isLeft(), true);
+    expect(result?.fold(id, id), isA<InvalidTextError>());
+    result = await usecase("");
+    expect(result?.fold(id, id), isA<InvalidTextError>());
+
     //expect(result!.isLeft(), true);
     // expect(result.fold((l) => l, (r) => r)
     // expect(result!.fold(id, id), isA<InvalidTextError>()); //id e do dartz
